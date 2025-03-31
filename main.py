f79fa18b7de5af1112ead458fd0262aca7360447
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-03-31 20:10:59 +0400

    Added list headers

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import bs4
 from fake_headers import Headers
 
 
+KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+
 responce = requests.get(url="https://habr.com/ru/companies/selectel/articles/",
                         headers=Headers(browser="chrome", os="mac").generate())
-print(responce.request.headers)
+
+soup = bs4.BeautifulSoup(responce.text, features="lxml")
+article_list = soup.find_all("article")
+
+
+for article in article_list:
+    '''Находим список заголовков'''
+    pass
